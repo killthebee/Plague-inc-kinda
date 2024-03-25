e10dfb7d4889517503f8e0df39f65de4ea93fce9
killthebee <danny.developer@ro.ru>
2024-03-25 19:37:25 +0300

    delete storyboards, init setup

--- Plague/ViewController.swift
+++ Plague/ViewController.swift
@@ -1,17 +1,10 @@
-//
-//  ViewController.swift
-//  Plague
-//
-//  Created by admin on 25.03.2024.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+class StartViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .cyan
     }
 
 
